ee980adc209b7d2a4910bcede1ed8fc2228d15f8
MaxrodrigoSS <max.rodrigo2001@gmail.com>
2019-12-16 17:39:09 -0300

    novos placares
    Add volei, futebol e basquete placar.

---
+++ OlimpiadasdeESP/app/src/main/java/com/maxrodrigoss/olimpiadasdeesp/PlacarBasquete.kt
@@ -0,0 +1,12 @@
+package com.maxrodrigoss.olimpiadasdeesp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlacarBasquete : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_placar_basquete)
+    }
+}
---
+++ OlimpiadasdeESP/app/src/main/java/com/maxrodrigoss/olimpiadasdeesp/PlacarVolei.kt
@@ -0,0 +1,12 @@
+package com.maxrodrigoss.olimpiadasdeesp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlacarVolei : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_placar_volei)
+    }
+}
